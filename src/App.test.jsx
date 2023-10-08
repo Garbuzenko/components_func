e319f75084aebffe1e6e96ac831ca10ce944e000
Garbuzenko <mike.garbuzenko@gmail.com>
2023-10-08 04:00:25 +0300

    ok

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@
 // import App from './App';
 
 // test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Tiger of Sweden/i);
-//   expect(linkElement).toBeInTheDocument();
+  // render(<App />);
+  // const linkElement = screen.getByText(/Tiger of Sweden/i);
+  // expect(linkElement).toBeInTheDocument();
 // });
